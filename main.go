8572876916371c1d1a5193bda48263eb908fb82d
Lenovo 15 <information@com1software.com>
2024-06-07 13:11:11 -0400

    update

--- main.go
+++ main.go
@@ -2,35 +2,35 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"runtime"
 	"io/ioutil"
-        "log"
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
 )
 
 func main() {
 	fmt.Printf("ffmpeg Parser - (c) Copyright Com1Software 1992-2024\n")
 	fmt.Printf("Operating System : %s\n", runtime.GOOS)
-	exefile := "ffmpeg"
-
-	if CheckForFile(exefile) {
+	exefile := "/ffmpeg/bin/ffmpeg.exe"
+	wdir := "/tmp/"
+	if _, err := os.Stat(exefile); err == nil {
 		fmt.Printf("- Parser Detected")
-	        files, err := ioutil.ReadDir("/tmp/")
-                if err != nil {
-                  log.Fatal(err)
-                }
-                for _, file := range files {
-                     fmt.Println(file.Name(), file.IsDir())
-                }
+		files, err := ioutil.ReadDir(wdir)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, file := range files {
+			fmt.Println(file.Name(), file.IsDir())
+
+			cmd := exec.Command(exefile, "-i", wdir+file.Name(), "-vframes", "1", "-s", "640x480", wdir+file.Name()+".png")
+			fmt.Println(cmd)
+			if err := cmd.Run(); err != nil {
+				fmt.Println("Error: ", err)
+			}
+		}
+	} else {
+		fmt.Println(err)
 	}
 
-}
-
-func CheckForFile(path string) bool {
-	file, err := os.Open(path)
-	if err != nil {
-		return false
-	}
-	file.Close()
-	return true
 }
